878972c02875e55aa252446d3f5f344271054ef5
Hannah <hi@loon-design.de>
2023-08-18 20:14:02 +0200

    Add several Pokémon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
 let pokemonList = [];
+pokemonList = [
+    {
+        name: 'Bulbasaur',
+        type: ['Grass', 'Poison'],
+        species: 'Seed Pokémon',
+        height: '0.7',
+        abilities: ['Chlorophyll', 'Overgrow']
+    },
+
+    {
+        name: 'Charmander',
+        type: ['Fire'],
+        species: 'Lizard Pokémon',
+        height: '0.6',
+        abilities: ['Blaze', 'Solar-Power']
+    },
+
+    {
+        name: 'Squirtle',
+        type: ['Water'],
+        species: 'Young Turtle Pokémon',
+        height: '0.5',
+        abilities: ['Rain-Dish', 'Torrent']
+    },
+
+    {
+        name: 'Pidgey',
+        type: ['Flying', 'Normal'],
+        species: 'Small Bird Pokémon',
+        height: '0.3',
+        abilities: ['Keen-Eye', 'Tangled-Feet', 'Big-Pecks']
+    },
+
+    {
+        name: 'Pikachu',
+        type: ['Electric'],
+        species: 'Mouse Pokémon',
+        height: '0.4',
+        abilities: ['Static', 'Lightningrod']
+    }
+]
